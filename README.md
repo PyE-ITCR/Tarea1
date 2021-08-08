8f82a43562fe3fc063066e267261f50520866729
Agustín V <joseagustinveneveg@gmail.com>
2021-08-07 22:30:37 -0600

    [ADD] Adición de archivos de especificación y desarrollo.
    Se añadieron los archivos necesarios para el entendimiento y desarrollo de la tarea. Además una breve descripción en el README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Tarea1
+﻿# Tarea1
 Este repositorio tiene como fin la realización de la Tarea #1 del curso de Probabilidad y Estadística GR 3 del segundo semestre del año 2021.
+
+Para más información leer “Tareas_tarea-1_Tarea01.pdf”.
---
+++ template_tarea1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Curso Proabilidad y Estadística
+
+Tarea #1
+
+Template con lectura de datos en archivo csv
+
+"""
+
+import numpy as np
+
+
+input_dir='C:/Users/PATH/' #PATH al archivo de datos, cambiar según cada computadora. Sirve para evitar 'File not found'
+filename=input_dir+'energydata_complete.csv'
+
+# Esta línea lee la matriz de datos (sin titulos) que van desde T1 hasta RH_9 y funciona para números solamente.
+# En su forma actual, esta línea NO LEE datos de tiempo. Para cambios en el mismo, usar la referencia de la funcion genfromtxt como guia
+datos=np.genfromtxt(filename,delimiter=';',skip_header=1, usecols = range(3,21))
+
+# Su código va aquí...
